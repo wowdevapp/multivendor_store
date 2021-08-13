240ca155902b6342a80d6348044a41e81e459c01
wowdevapp <m.eng.abdelhaq@gmail.com>
2021-08-13 01:21:47 +0100

    end branch settings

--- routes/admin.php
+++ routes/admin.php
@@ -29,6 +29,11 @@ Route::group([
             Route::get('shipping-methods/{type}', 'SettingsController@editShippingMethods')->name('edit.shippings.methods');
             Route::put('shipping-methods/{id}', 'SettingsController@updateShippingMethods')->name('update.shippings.methods');
         });
+        Route::group(['prefix' => 'profile'], function () {
+            Route::get('edit', 'ProfileController@editProfile')->name('edit.profile');
+            Route::put('update', 'ProfileController@updateProfile')->name('update.profile');
+            //Route::put('update', 'ProfileController@updatePassword')->name('update.password');
+        });
 
     });
 
