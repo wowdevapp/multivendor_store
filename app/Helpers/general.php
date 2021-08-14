90eedc24a451419e16248cdaef9acfb5e7b9d523
wowdevapp <m.eng.abdelhaq@gmail.com>
2021-08-14 12:40:45 +0100

    add brands and brand image

--- app/Helpers/general.php
+++ app/Helpers/general.php
@@ -4,3 +4,9 @@ define('PAGINATION_COUNT',15);
 
      return app() -> getLocale() == 'ar' ? 'css-rtl' : 'css';
 }
+
+function uploadImage($folder,$image){
+    $image->store('/', $folder);
+    $filename = $image->hashName();
+    return  $filename;
+ }
--- resources/views/dashboard/includes/sidebare.blade.php
+++ resources/views/dashboard/includes/sidebare.blade.php
@@ -55,16 +55,16 @@
             </li>
 
             <li class="nav-item"><a href=""><i class="la la-male"></i>
-                    <span class="menu-title" data-i18n="nav.dash.main">المتاجر  </span>
+                    <span class="menu-title" data-i18n="nav.dash.main">الماركات  </span>
                     <span
-                        class="badge badge badge-success badge-pill float-right mr-2"> </span>
+                        class="badge badge badge-success badge-pill float-right mr-2">{{App\Models\Brand::count()}}</span>
                 </a>
                 <ul class="menu-content">
-                    <li class="active"><a class="menu-item" href=" "
+                    <li class="active"><a class="menu-item" href="{{route("admin.brands")}} "
                                           data-i18n="nav.dash.ecommerce"> عرض الكل </a>
                     </li>
-                    <li><a class="menu-item" href=" " data-i18n="nav.dash.crypto">أضافة
-                            متجر  </a>
+                    <li><a class="menu-item" href="{{route("admin.brands.create")}}" data-i18n="nav.dash.crypto">أضافة
+                            ماركة  </a>
                     </li>
                 </ul>
             </li>
